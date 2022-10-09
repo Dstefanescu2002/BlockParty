3e64d23766cd979c7f1ae183437983e2aacc0e29
Dstefanescu2002 <dstefanescu1609@gmail.com>
2022-10-09 14:07:59 -0400

    Added commenting and mongoose

--- back-end/index.js
+++ back-end/index.js
@@ -1,17 +1,23 @@
 const express = require('express')
 const app = express()
+// Middleware
 app.use(express.json());
+// MongoDB/Mongoose initialization
+const mongoose = require("mongoose");
 
 let users = []
 
+// Get the homepage, will have basic information
 app.get('/', (req, res) => {
     res.send('<h1>Block Party Backend API</h1><br /><p>Welcome to the Block Party Backend!!!</p>')
 })
 
+// Get all users
 app.get('/users', (req, res) => {
     res.json(users)
 })
 
+// Add a user
 app.post('/users', (req, res) => {
     users.push(req.body)
     res.send('POST request to the homepage')
